8988c700a4a8484bff924c83938808151276a0c1
Omar <amidurashid@gamil.com>
2024-08-06 02:38:24 +0000

    Feat: Added a dataHandler

---
+++ utils/dataHandler.js
@@ -0,0 +1,20 @@
+import { Low } from "lowdb"
+import { JSONFile } from 'lowdb/node'
+
+export const createData = (dName, filePath)=>{
+    let defaultData = { }
+    const newName = dName;
+    const newValue = [];
+    defaultData[newName] = newValue
+    const adapter = new JSONFile(filePath)
+    const db = new Low(adapter, defaultData)
+    return db
+
+}
+
+// createData("users", "file")
+
+
+
+
+
